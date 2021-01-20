e52877626171448708ea7628052b7ea51758445a
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-20 13:39:50 +0200

    Add function that doesn't reveal intent

--- meaningful-names.playground/Contents.swift
+++ meaningful-names.playground/Contents.swift
@@ -6,3 +6,15 @@ var elapsedTimeInDays: Int
 var daysSinceCreation: Int
 var daysSinceModification: Int
 var fileAgeInDays: Int
+
+var theList = [[Int]]()
+
+public func getThem() -> [[Int]] {
+    var list1 = [[Int]]()
+    for x in theList {
+        if x[0] == 4 {
+            list1.append(x)
+        }
+    }
+    return list1
+}
