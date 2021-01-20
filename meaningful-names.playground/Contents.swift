aba69c75766de049775fa10151356faec7ba79db
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-20 13:28:06 +0200

    Add intention revealing variable names

--- meaningful-names.playground/Contents.swift
+++ meaningful-names.playground/Contents.swift
@@ -1,1 +1,8 @@
+// MARK: - Use Intention-Revealing Names
 
+var d: Int // elapsed time in days
+
+var elapsedTimeInDays: Int
+var daysSinceCreation: Int
+var daysSinceModification: Int
+var fileAgeInDays: Int
