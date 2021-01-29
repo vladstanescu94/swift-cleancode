d34ee414cee5eb072e5d1109c4a266c8f557de32
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-29 10:03:42 +0200

    Add example of comment that expains intent

--- comments.playground/Contents.swift
+++ comments.playground/Contents.swift
@@ -17,3 +17,21 @@ var employee = Employee(flags: true, age: 20)
 
 if employee.isEligibleForFullBenefits() { }
 
+// MARK: - Explanation of Intent
+
+struct WikiPagePath {
+    var names: [String]
+}
+
+let names = ["name1", "name2"]
+
+func compareTo(o: Any) -> Int {
+    if let obj = o as? WikiPagePath {
+        let compressedName = names.joined(separator: "")
+        let compressedArgumentNmae = obj.names.joined(separator: "")
+        return compressedName == compressedArgumentNmae ? 1 : 0
+    }
+    return 1 // we are greater because we are the right type
+}
+
+
