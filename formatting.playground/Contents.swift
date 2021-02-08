144f81ed77905f1c61285873835895fe05beb5d9
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-08 12:55:35 +0200

    Remove useless comments

--- formatting.playground/Contents.swift
+++ formatting.playground/Contents.swift
@@ -3,13 +3,7 @@
 public struct Property { }
 
 public struct ReporterConfig {
-    /**
-        The class name of the reporter listener
-     */
     private var m_className: String
-    /**
-        The properties of the reporter listener
-     */
     public var m_properties = [Property]()
     
     public mutating func addProperty(property: Property) {
