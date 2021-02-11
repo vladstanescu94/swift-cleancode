6fe8838599294aa861d1fcb087d13237b4906113
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-11 10:19:06 +0200

    Add small example of dependent functions that are vertically close

--- formatting.playground/Contents.swift
+++ formatting.playground/Contents.swift
@@ -10,3 +10,19 @@ public struct ReporterConfig {
         m_properties.append(property)
     }
 }
+
+// Mark: - Dependent functions
+
+func generateSomething(from array: [Int]) -> Int {
+    let number = getFirstNumber(from: array)
+    let sum = number + getRandomNumber()
+    return sum
+}
+
+func getFirstNumber(from array: [Int]) -> Int {
+    return array[0]
+}
+
+func getRandomNumber() -> Int {
+    return Int.random(in: 0...10)
+}
